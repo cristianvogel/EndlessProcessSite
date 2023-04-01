8d338bc565daaf09808a0414faceba6862208d01
cristianvogel <cristian.vogel55@gmail.com>
2023-04-02 01:07:54 +0200

    Added a ▼

--- endless-process-label-site/src/routes/+page.svelte
+++ endless-process-label-site/src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
 	// Landing page
-	import { Icon } from '@steeze-ui/svelte-icon';
-	import { CaretSortDown  } from '@steeze-ui/carbon-icons';
 	import {CablesPatch, CablesAudioFileURL} from '$lib/stores/stores';
 </script>
 
@@ -9,7 +7,7 @@
 <div class="w-full  p-1 bg-transparent text-xs" >
 	<!-- possibility for an audio player here -->
 		
-			<a href="/blog"><Icon src={CaretSortDown} class="h-8 animate-pulse"/></a>
+			
 			{#if $CablesPatch}
 			<span class='text-sm'>
 				Patch initialised : Playing {$CablesAudioFileURL[0]}
