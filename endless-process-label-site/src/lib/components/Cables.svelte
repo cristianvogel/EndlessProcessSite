62341b2a79c9391dc3507c911796b414f9950a09
cristianvogel <cristian.vogel55@gmail.com>
2023-04-03 18:43:05 +0200

    Cables -> Elementary finally working. Refactored AudioEngine reactivity to new model.

--- endless-process-label-site/src/lib/components/Cables.svelte
+++ endless-process-label-site/src/lib/components/Cables.svelte
@@ -1,26 +1,42 @@
 <script lang="ts">
 	
 	import { 
-			CablesAudioContext, 
 			CablesAudioFileURL, 
-			ElementaryAudioEngine, 
 			CablesPatch,
-			CablesText } from '$lib/stores/stores';
+			CablesText
+		} from '$lib/stores/stores';
+	import { Audio } from '$lib/stores/AudioEngine';
 	import { onMount } from 'svelte';
-	import { get } from 'svelte/store';
 	import { Utils } from '$lib/classes/Utils';
+	import { tick } from 'svelte';
 
 	export let patch: string;
 	export let bg: boolean = false;
 	export let spin: boolean = false;
 
+	const { endNodes } = Audio.stores
+		
 	let pathPatch: string = `src/lib/cables/${patch}/patch.js`;	
 	$: loadedTrack =  $CablesAudioFileURL[0] 
 	$: if (spin) { 
-		$CablesText[0] = Utils.rotateString($CablesText[0])
-		$CablesText[1] = Utils.rotateString($CablesText[1])
+		CablesText.set( [ Utils.rotateString($CablesText[0]), Utils.rotateString($CablesText[1]) ] )
 		spinText($CablesText) 
 	}
+	
+	$: tick().then( ()=> {
+		if (typeof $CablesPatch !== 'string' ) {
+			$CablesPatch.getVar('CablesGainNode').on("change", 
+			(function( newValue:GainNode ) {
+				if (!newValue || $endNodes.cables != null ) return
+				console.log ( 'Patched valid Cables end node', newValue)
+				Audio.cablesEndNode = newValue;	
+				Audio.connectEndNodes();
+   			 }))
+		} else {
+			// spinner whilst CablesPatch loads?
+			CablesText.set( [ Utils.rotateString($CablesText[0]), Utils.rotateString($CablesText[1]) ] )
+		}
+	})
 	
 
 	const initializeCables = () => {
@@ -35,7 +51,6 @@
 			onPatchLoaded: patchInitialized,
 			onFinishedLoading: patchFinishedLoading,
 			canvas: { alpha: true, premultipliedAlpha: true },
-			masterVolume: 0.707,
 			variables:{
 				"CablesAudioFileURL": loadedTrack,
 				"CablesMute": false,
@@ -50,34 +65,30 @@
 	}
 
 	function patchInitialized() {
-		// You can now access the patch object (CABLES.patch), register variable watchers and so on
-		CablesAudioContext.set(CABLES.WEBAUDIO.getAudioContext());
-		console.log('CablesAudioContext: ', $CablesAudioContext);
-		$ElementaryAudioEngine.init($CablesAudioContext);
-			console.log('Elem AudioEngine initialized with CablesAudioContext');
-		get(CablesAudioContext).resume();
-	
-	}
+		console.log('Cables Patch initialized');
+		Audio.init(CABLES.WEBAUDIO.getAudioContext());
+		console.log('Initialising AudioEngine (with CablesAudioContext) and', Audio.audioStatus);			
+	} 
 
 	function patchFinishedLoading() {
 		spinText(["Endless", "Process"])
 	}
 
-	function spinText(  prompts:string[] = ["",""]  ) {
+	function spinText(  prompts:string[] = ["End","Proc"]  ) {
 	if ( typeof $CablesPatch !== 'string' ) {
-			$CablesPatch.config.spinAndPrompt("",prompts[0],prompts[1])
+			$CablesPatch.config.spinAndPrompt('',prompts[0],prompts[1]) // bug in Cables won't pass first arg
 		}
 	}
-onMount(() => {
-	initializeCables()
-})
+
+	onMount(() => {
+		initializeCables();
+	})
 	
 	</script>
 
 <svelte:head>
-	<script src={pathPatch} on:load={initializeCables}></script>
-
- 
+	<!-- todo: move this data load server side -->
+	<script src={pathPatch}></script>
 </svelte:head>
 
 <div class="mb-4 ">
@@ -88,7 +99,3 @@ onMount(() => {
 		style="width: 100%; height: 100%; z-index: {bg? -137: 0}; position: fixed;"
 	/>
 </div>
-
-<style>
-
-</style>
